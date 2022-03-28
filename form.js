394701c38987b3ef932b0c9723af2cba94fc8d21
Mateusz Kidawa <mateusz.kidawa@gmail.com>
2022-03-28 16:59:49 +0200

    form final version

--- form.js
+++ form.js
@@ -1,53 +1,88 @@
-// Połącz się z buttonem
-const btn = window.document.getElementById("submit");
-const btn = window.document.forms['przycisk'];
-
 const form = document.querySelector("form");
-const input = form.querySelector("input");
+const btn =
+  document.getElementById("submit");
+const btn2 = document.getElementById("btn2");
+const showModal =
+  document.getElementById("modal");
+const closingModal =
+  document.getElementById("close");
 
-
-// Jeśli event klik na nim, to console.log
-form.addEventListener("submit", e => {
-    e.preventDefault();
-
-    //jeżeli wszystko ok to wysyłamy
-    if (input.value.length >= 3) {
-        // e.target.submit();
-    } else {
-        //jeżeli nie to pokazujemy jakieś błędy
-        alert("Kolego wypełniłeś błędnie nasz super formularz");
-    }
-});
-
-{
-    const firstNameElement = document.querySelector(".name");
-    const contactFormElement = document.querySelector(".form");
-
-    function GetValues(firstName) {
-        this.firstName = firstName;
-    };
-
-    const logInConsole = (obj) => {
-        console.log(`Twoje imie: ${obj.firstName}`);
-    };
-
-    contactFormElement.addEventListener("submit", (event) => {
-        event.preventDefault();
-
-        const formValues = new GetValues(
-            firstNameElement.value,
-        );
-    });
+function nameVer(data) {
+  const pattern = /^[A-Za-z]*$/;
+  //length && pattern
+  if (
+    data.length < 2 ||
+    !pattern.test(data)
+  ) {
+    alert(`Wrong Prénom/Nom ${data} input`);
+    return 0;
+  } else {
+    return 1;
+  }
+}
+function phoneVer(data) {
+  const pattern = /[0-9]{3}[0-9]{3}[0-9]{3}/;
+  if (!pattern.test(data)) {
+    alert(`Wrong Téléphone ${data} input`);
+    return 0;
+  } else {
+    return 1;
+  }
+}
+function mailVer(data) {
+  const pattern =
+    /^[-\w\.]+@([-\w]+\.)+[a-z]+$/i;
+  if (!pattern.test(data)) {
+    alert(
+      `Wrong Adresse mail ${data} input`
+    );
+    return 0;
+  } else {
+    return 1;
+  }
+}
+function msgVar(data) {
+  const pattern = /^[A-Za-z]*$/;
+  if (!pattern.test(data)) {
+    alert(`Wrong Message ${data} input`);
+    return 0;
+  }  else {
+    return 1;
+  }
 }
 
-// Połącz się z buttonem
-const btn = window.document.getElementById("submit");
+function verification() {
+  if (
+    nameVer(form["user_name"].value) &&
+    nameVer(form["user_surname"].value) &&
+    phoneVer(form["user_phone"].value) &&
+    mailVer(form["user_mail"].value) &&
+    msgVar(form["user_msg"].value)
+  ) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
 
-// Jeśli event klik na nim, to console.log
-btn.addEventListener("click", () => {
-    const payload = {
-        type: "BUTTON_CLICK",
-        timestamp: Date.now()
-    }
-    console.log(payload);
-})
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const value = verification();
+  console.log(value);
+  if (value) {
+    showModal.style.display = "block";
+  }
+  clearForm();
+});
+btn2.addEventListener("click", (e) => {
+  e.preventDefault();
+  showModal.style.display = "none";
+});
+
+function clearForm() {
+  form["user_name"].value = "";
+  form["user_surname"].value = "";
+  form["user_phone"].value = "";
+  form["user_mail"].value = "";
+  form["user_msg"].value = "";
+}
